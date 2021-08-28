20fc50cc8f7dd0268646357f1b6f8a9bbd3f275a
okatsuo <rafael.goncanna@gmail.com>
2021-08-28 17:26:49 -0300

    feat: add default container to app

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,18 +1,21 @@
+import { Container } from '../container'
 import * as Styles from './styles'
 
 const Login = () => {
   return (
     <Styles.Wrapper>
-      <Styles.Content>
-        <Styles.Square>
-          <Styles.Items>
-            <input type="text" name="email" id="email" placeholder="email"/>
-            <input type="password" name="password" id="password" placeholder="senha"/>
-            <input type="checkbox" name="rememberMe" id="rememberMe"/> lembrar-me
-            <input type="button" value="ENTRAR" />
-          </Styles.Items>
-        </Styles.Square>
-      </Styles.Content>
+      <Container>
+        <Styles.Content>
+          <Styles.Square>
+            <Styles.Items>
+              <input type="text" name="email" id="email" placeholder="email"/>
+              <input type="password" name="password" id="password" placeholder="senha"/>
+              <input type="checkbox" name="rememberMe" id="rememberMe"/> lembrar-me
+              <input type="button" value="ENTRAR" />
+            </Styles.Items>
+          </Styles.Square>
+        </Styles.Content>
+      </Container>
     </Styles.Wrapper>
   )
 }

e60c06c7d061e129897470df43567588cd665da6
okatsuo <rafael.goncanna@gmail.com>
2021-09-05 00:30:46 -0300

    feat: create basic header structure

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,27 @@
+import { parseCookies } from 'nookies'
+import { jwtDecode } from '../../utils/JWT'
+import { Container } from '../container'
+import * as Styles from './styles'
 
 const Header = () => {
+  const cookies = (parseCookies()).t_user
+  const user = jwtDecode(cookies) // TODO remember to change the backend to return JWT with email as well
   return (
-    <p>saaalve</p>
+    <Styles.Wrapper>
+      <Container>
+        <Styles.Container>
+          <Styles.UserInfo>
+            <div>
+              email@example.com
+            </div>
+            <div>
+              sair
+            </div>
+          </Styles.UserInfo>
+        </Styles.Container>
+      </Container>
+    </Styles.Wrapper>
+
   )
 }
 
---
+++ src/components/header/styles.ts
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.nav`
+${({ theme }) => css`
+background: ${theme.colors.secondaryBackground};
+`}
+`
+
+export const Container = styled.div`
+  padding: 10px 10px;
+`
+
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
+`

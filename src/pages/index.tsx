4a8731b84e5b79561c99148610a363f873ef1556
okatsuo <rafael.goncanna@gmail.com>
2021-09-05 01:44:59 -0300

    feat: create privated route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next'
 import Header from '../components/header'
+import isLogged from '../utils/authentication'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
     <Header />
   )
+}
+
+export const getServerSideProps = async (context: any) => {
+  if (!isLogged(context)) {
+    return {
+      redirect: { destination: '/login', permanent: false }
+    }
+  }
+  return {
+    props: {
+      msg: 'tomanocu'
+    }
+  }
 }
 
 export default Home
